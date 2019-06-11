74835a59004433c324a98766d074ab3581fc25bb
calebredd <caleb.redd@yahoo.com>
2019-06-10 19:45:00 -0700

    Added article toggle function when expand buttons are clicked

--- Article/Article.js
+++ Article/Article.js
@@ -3,27 +3,26 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement=domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton=domElement.querySelector(".expandButton");
+    this.expandButton.innerHTML="expand";
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.addEventListener("click",this.expandArticle);
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    return event.target.parentElement.classList.toggle("article-open");
   }
 }
 
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-*/
-
-let articles;
+// START HERE: 
+let articles=document.querySelector(".articles").querySelectorAll(".article");
+//- Select all classes named ".article" and assign that value to the articles variable.  
+articles.forEach(function(e){
+  return new Article(e);
+});
+//- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
