84f83eb5713a1217fa32f99ea23f3f3f34d48fe7
Caleb Redd <caleb.redd@yahoo.com>
2019-07-16 19:30:22 -0700

    Took out code from previous project version as it was no longer needed, cleaned up file

--- Article/Article.js
+++ Article/Article.js
@@ -96,40 +96,6 @@ const data = [
     thirdParagraph: `Back to school again Maxwell plays the fool again. Teacher gets annoyed....... But as she's getting ready to go, a knock comes from the door. `
   }
 ];
-// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
-// class Article {
-//   constructor(domElement) {
-//     // assign this.domElement to the passed in domElement
-//     this.domElement = domElement;
-//     // create a reference to the ".expandButton" class.
-//     this.expandButton = domElement.querySelector(".expandButton");
-//     this.expandButton.innerHTML = "expand";
-//     // Using your expandButton reference, update the text on your expandButton to say "expand"
-//     this.expandButton.addEventListener("click", this.expandArticle);
-//     // Set a click handler on the expandButton reference, calling the expandArticle method.
-//   }
-
-//   expandArticle() {
-//     // Using our reference to the domElement, toggle a class to expand or hide the article.
-//     event.target.parentElement.classList.toggle("article-open");
-//     event.target.parentElement.classList.toggle("open");
-//     if (event.target.innerHTML === "expand") {
-//       return (event.target.innerHTML = "collapse");
-//     } else {
-//       return (event.target.innerHTML = "expand");
-//     }
-
-//     // START HERE:
-//     let articles = document
-//       .querySelector(".articles")
-//       .querySelectorAll(".article");
-//     //- Select all classes named ".article" and assign that value to the articles variable.
-//     articles.forEach(function(e) {
-//       return new Article(e);
-//     });
-//   }
-// }
 
 class Article {
   constructor(articleData) {
@@ -152,13 +118,13 @@ class Article {
     var thirdParagraph = document.createElement("p");
     var expandButton = document.createElement("span");
     expandButton.classList.add("expandButton");
-    expandButton.textContent="Expand";
-    expandButton.addEventListener("click",function(){
+    expandButton.textContent = "Expand";
+    expandButton.addEventListener("click", function() {
       article.classList.toggle("article-open");
-      if(expandButton.textContent=="Expand"){
-        expandButton.textContent="Collapse";
-      }else{
-        expandButton.textContent="Expand";
+      if (expandButton.textContent == "Expand") {
+        expandButton.textContent = "Collapse";
+      } else {
+        expandButton.textContent = "Expand";
       }
     });
 
